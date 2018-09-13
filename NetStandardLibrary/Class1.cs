99482cbad0289d129ac6b0aeec888a87082eac30
Santiago Fernandez Madero <safern@microsoft.com>
2018-09-13 11:29:23 -0700

    Initial commit

---
+++ NetStandardLibrary/Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+using System.Reflection;
+using System.Reflection.Emit;
+
+namespace NetStandardLibrary
+{
+    public class Class1
+    {
+        public static void TestDefineParameters()
+        {
+            Type[] mathArgs = { typeof(double), typeof(double) };
+
+            var powerOf = new DynamicMethod("PowerOf",
+                typeof(double),
+                mathArgs,
+                typeof(double).Module);
+
+            ILGenerator il = powerOf.GetILGenerator(256);
+            il.Emit(OpCodes.Ldarg_0);
+            il.Emit(OpCodes.Ldarg_1);
+            il.Emit(OpCodes.Call, typeof(Math).GetMethod("Pow"));
+            il.Emit(OpCodes.Ret);
+            
+            powerOf.DefineParameter(1, ParameterAttributes.In, "base");
+            powerOf.DefineParameter(2, ParameterAttributes.Out, "exponent");
+            
+            object[] invokeArgs = { 2, 5 };
+            object objRet = powerOf.Invoke(null, BindingFlags.ExactBinding, null, invokeArgs, new CultureInfo("en-us"));
+
+            foreach (var parameter in powerOf.GetParameters())
+            {
+                Console.WriteLine($"Param: {parameter.Name} -- In?: {parameter.IsIn} -- Out?: {parameter.IsOut}");
+            }
+        } 
+    }
+}
